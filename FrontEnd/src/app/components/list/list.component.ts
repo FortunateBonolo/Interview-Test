7a730e9def01c0033b53a4feebb0211943cae514
FortunateBonolo <fortunatemahlalela@gmail.com>
2023-12-10 20:22:04 +0200

    added get contacts to  list component

--- FrontEnd/src/app/components/list/list.component.ts
+++ FrontEnd/src/app/components/list/list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service'; 
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  contacts: any[] = [];
 
-  ngOnInit() {
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.getContacts();
   }
 
+  getContacts(): void {
+    this.apiService.getContacts().subscribe(
+      (data: any) => {
+        this.contacts = data;
+        console.log('Contacts:', this.contacts);
+      },
+      error => {
+        console.error('Error fetching contacts:', error);
+      }
+    );
+  }
 }
