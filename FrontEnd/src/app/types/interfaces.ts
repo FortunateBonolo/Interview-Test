c014b36cb16e84d1403af18754ea3b4583fa0887
FortunateBonolo <fortunatemahlalela@gmail.com>
2023-12-11 11:14:24 +0200

    fixing the interface

--- FrontEnd/src/app/types/interfaces.ts
+++ FrontEnd/src/app/types/interfaces.ts
@@ -1,7 +1,30 @@
-export interface Hero {
-    name:string;
-    power:string;
-    stats: { key: string, value: number }[];
-    evolve(statIncrease?: number): void;
-}
-
+export interface IHero {
+    name: string;
+    power: string;
+    stats: KeyValuePair<string, number>[];
+    evolve(): void;
+  }
+  
+  export class Hero implements IHero {
+    name: string;
+    power: string;
+    stats: KeyValuePair<string, number>[] = [];
+  
+    constructor(name: string, power: string, stats: KeyValuePair<string, number>[] = []) {
+      this.name = name;
+      this.power = power;
+      this.stats = stats;
+    }
+  
+    evolve(): void {
+      this.stats.forEach(stat => {
+        stat.value += 0.5 * stat.value;
+      });
+    }
+  }
+  
+  export interface KeyValuePair<K, V> {
+    key: K;
+    value: V;
+  }
+  
