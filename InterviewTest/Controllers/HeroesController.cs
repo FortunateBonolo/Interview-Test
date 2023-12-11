e67610f1c97579f796b7e29b48dd6839ad722de0
FortunateBonolo <fortunatemahlalela@gmail.com>
2023-12-11 11:27:36 +0200

    post method should read an action parameter

--- InterviewTest/Controllers/HeroesController.cs
+++ InterviewTest/Controllers/HeroesController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,51 +9,64 @@ namespace InterviewTest.Controllers
     [ApiController]
     public class HeroesController : ControllerBase
     {
-        private Hero[] heroes = new Hero[] {
-               new Hero()
-               {
-                   name= "Hulk",
-                   power="Strength from gamma radiation",
-                   stats=
-                   new List<KeyValuePair<string, int>>()
-                   {
-                       new KeyValuePair<string, int>( "strength", 5000 ),
-                       new KeyValuePair<string, int>( "intelligence", 50),
-                       new KeyValuePair<string, int>( "stamina", 2500 )
-                   }
-               }
-            };
+        private Hero[] heroes = new Hero[]
+        {
+            new Hero()
+            {
+                name= "Hulk",
+                power="Strength from gamma radiation",
+                stats= new List<KeyValuePair<string, int>>()
+                {
+                    new KeyValuePair<string, int>( "strength", 5000 ),
+                    new KeyValuePair<string, int>( "intelligence", 50),
+                    new KeyValuePair<string, int>( "stamina", 2500 )
+                }
+            }
+        };
 
         // GET: api/Heroes
         [HttpGet]
         public IEnumerable<Hero> Get()
         {
-            return this.heroes;
+            return heroes;
         }
 
         // GET: api/Heroes/5
         [HttpGet("{id}", Name = "Get")]
         public Hero Get(int id)
         {
-            return this.heroes.FirstOrDefault();
+            return heroes.FirstOrDefault();
         }
 
         // POST: api/Heroes
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] string value, [FromQuery] string action = "none")
         {
+            if (action.ToLower() == "evolve")
+            {
+                // Evolve the hero and return the updated hero
+                heroes[0].evolve();
+                return Ok(heroes[0]);
+            }
+            else
+            {
+                // Handle other actions or default behavior
+                return BadRequest("Invalid action parameter");
+            }
         }
 
         // PUT: api/Heroes/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            // Update hero logic if needed
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            // Delete hero logic if needed
         }
     }
 }
